a8f2a8160ba2b8083dfad113f9e2257c3b25f250
carezzacreaciones-cloud <carezzacreaciones@gmail.com>
2026-03-28 12:06:34 -0300

    subir archivos iniciales de la rifa

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// 1) Copia este archivo como firebase-config.js
+// 2) Reemplaza los valores con los de tu proyecto Firebase
+// 3) NO subas firebase-config.js si no quieres exponer tu configuración en un repo privado
+//    (en sitios web públicos normalmente sí queda visible, eso es normal en Firebase Web)
+
+// Import the functions you need from the SDKs you need
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAa8eKjCh581w6s55eKi3qzua6Qhbtbv6A",
+  authDomain: "carezza-rifa.firebaseapp.com",
+  projectId: "carezza-rifa",
+  storageBucket: "carezza-rifa.firebasestorage.app",
+  messagingSenderId: "255670318747",
+  appId: "1:255670318747:web:cffa2802eb17b89a73f83e"
+};
+
+window.RAFFLE_ID = "gran-rifa";
